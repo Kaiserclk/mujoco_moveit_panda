d7ec64df4eb8260c50d00d718316fde34f23b99a
kaiser <1942797709@qq.com>
2026-05-20 18:38:02 +0800

    5.20同步

--- moveit_servo/include/moveit_servo/utils/command.hpp
+++ moveit_servo/include/moveit_servo/utils/command.hpp
@@ -10,52 +10,54 @@ namespace moveit_servo
 {
 
 /**
- * \brief Compute the change in joint position for the given joint jog command.
- * @param command The joint jog command.
- * @param robot_state_ The current robot state as obtained from PlanningSceneMonitor.
- * @param servo_params The servo parameters.
- * @param joint_name_group_index_map Mapping between joint subgroup name and move group joint vector position.
- * @return The status and joint position change required (delta).
+ * @brief 根据给定的关节点动指令计算关节位置偏移量
+ * @param command 关节点动指令
+ * @param robot_state_ 从规划场景监视器获取的机器人当前状态
+ * @param servo_params 伺服控制参数
+ * @param joint_name_group_index_map 关节子组名称与运动组关节向量位置的映射关系
+ * @return 执行状态及所需的关节位置变化量（增量值）
  */
 JointDeltaResult jointDeltaFromJointJog(const JointJogCommand& command, const moveit::core::RobotStatePtr& robot_state,
                                         const servo::Params& servo_params,
                                         const JointNameToMoveGroupIndexMap& joint_name_group_index_map);
 
+
 /**
- * \brief Compute the change in joint position for the given twist command.
- * @param command The twist command.
- * @param robot_state_ The current robot state as obtained from PlanningSceneMonitor.
- * @param servo_params The servo parameters.
- * @param planning_frame The planning frame name.
- * @param joint_name_group_index_map Mapping between joint subgroup name and move group joint vector position.
- * @return The status and joint position change required (delta).
+ * @brief 根据给定的 twist 动指令计算关节位置偏移量
+ * @param command twist 动指令
+ * @param robot_state_ 从规划场景监视器获取的机器人当前状态
+ * @param servo_params 伺服控制参数
+ * @param planning_frame 计划坐标系名称
+ * @param joint_name_group_index_map 关节子组名称与运动组关节向量位置的映射关系
+ * @return 执行状态及所需的关节位置变化量（增量值）
  */
 JointDeltaResult jointDeltaFromTwist(const TwistCommand& command, const moveit::core::RobotStatePtr& robot_state,
                                      const servo::Params& servo_params, const std::string& planning_frame,
                                      const JointNameToMoveGroupIndexMap& joint_name_group_index_map);
 
 /**
- * \brief Compute the change in joint position for the given pose command.
- * @param command The pose command.
- * @param robot_state_ The current robot state as obtained from PlanningSceneMonitor.
- * @param servo_params The servo parameters.
- * @param planning_frame The planning frame name.
- * @param ee_frame The end effector frame name.
- * @param joint_name_group_index_map Mapping between sub group joint name and move group joint vector position
- * @return The status and joint position change required (delta).
+ * @brief 根据给定的位姿动指令计算关节位置偏移量
+ * @param command 位姿动指令
+ * @param robot_state_ 从规划场景监视器获取的机器人当前状态
+ * @param servo_params 伺服控制参数
+ * @param planning_frame 计划坐标系名称
+ * @param ee_frame 末端执行器坐标系名称
+ * @param joint_name_group_index_map 关节子组名称与运动组关节向量位置的映射关系
+ * @return 执行状态及所需的关节位置变化量（增量值）
  */
 JointDeltaResult jointDeltaFromPose(const PoseCommand& command, const moveit::core::RobotStatePtr& robot_state,
                                     const servo::Params& servo_params, const std::string& planning_frame,
                                     const std::string& ee_frame,
                                     const JointNameToMoveGroupIndexMap& joint_name_group_index_map);
 
+
 /**
- * \brief Computes the required change in joint angles for given Cartesian change, using the robot's IK solver.
- * @param cartesian_position_delta The change in Cartesian position.
- * @param robot_state_ The current robot state as obtained from PlanningSceneMonitor.
- * @param servo_params The servo parameters.
- * @param joint_name_group_index_map Mapping between joint subgroup name and move group joint vector position.
- * @return The status and joint position change required (delta).
+ * @brief 根据给定的笛卡尔坐标偏移量计算关节位置偏移量
+ * @param cartesian_position_delta 笛卡尔坐标偏移量
+ * @param robot_state_ 从规划场景监视器获取的机器人当前状态
+ * @param servo_params 伺服控制参数
+ * @param joint_name_group_index_map 关节子组名称与运动组关节向量位置的映射关系
+ * @return 执行状态及所需的关节位置变化量（增量值）
  */
 JointDeltaResult jointDeltaFromIK(const Eigen::VectorXd& cartesian_position_delta,
                                   const moveit::core::RobotStatePtr& robot_state, const servo::Params& servo_params,
